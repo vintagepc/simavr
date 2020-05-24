a0b69be1a7826675aa173d05591cb28861f26cc4
vintagepc <53943260+vintagepc@users.noreply.github.com>
2020-05-24 13:12:00 -0400

    fix state handling for input pins

--- simavr/sim/avr_ioport.c
+++ simavr/sim/avr_ioport.c
@@ -55,11 +55,12 @@ avr_ioport_update_irqs(
 	for (int i = 0; i < 8; i++) {
 		if (ddr & (1 << i))
 			avr_raise_irq(p->io.irq + i, (avr->data[p->r_port] >> i) & 1);
-		else if (p->external.pull_mask & (1 << i))
-			avr_raise_irq(p->io.irq + i, (p->external.pull_value >> i) & 1);
-		else if (((avr->data[p->r_port] >> i) & 1) && ((p->initial_set >>i)&1) )
+		else if (!((p->initial_set >>i)&1))
 		{
-			avr_raise_irq(p->io.irq + i, 1);
+			if (((p->external.pull_mask)>>i) & 1)
+				avr_raise_irq(p->io.irq + i, (p->external.pull_value >> i) & 1);
+			else if ((avr->data[p->r_port] >> i) & 1)
+				avr_raise_irq(p->io.irq + i, 1);
 			// Set the flag so we don't constantly clobber external input state - ~VintagePC
 			p->initial_set |= (1<<i);
 		}
@@ -171,6 +172,8 @@ avr_ioport_reset(
 	p->initial_set = 0;
 	for (int i = 0; i < IOPORT_IRQ_PIN_ALL; i++)
 		avr_irq_register_notify(p->io.irq + i, avr_ioport_irq_notify, p);
+	// force initial pu/pd state
+	avr_ioport_update_irqs(p);
 }
 
 static int
